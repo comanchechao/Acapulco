e2dc0f72eace46ed28e6c0ca25ca375bb773eeeb
comanchechao <61477751+comanchechao@users.noreply.github.com>
2020-12-01 03:32:14 +0330

    Backend Almost added

---
+++ server-middleware/api/data/index.js
@@ -0,0 +1,13 @@
+//api
+const axios = require('axios')
+const dataUrl = 'https://itunes.apple.com'
+
+module.exports = {
+  lookup(id) {
+    return axios.get(`${dataUrl}/lookup?id=${id}`)
+  },
+  search(value) {
+    //return axios.get(`${dataUrl}/search?${value}&limit=100`)
+    return axios.get(`${dataUrl}/search?${value}`)
+  }
+}
